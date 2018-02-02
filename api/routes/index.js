ebc771f35e31b716d00c544fb9c22839c5ad2b10
snuud <sn.snuud@gmail.com>
2018-02-02 17:32:02 +0800

    Ammended index.js in api

--- api/routes/index.js
+++ api/routes/index.js
@@ -11,7 +11,7 @@ router
 router
 .route('/property/:id')
 .get(PropertyController.getPropertyById)
-.delete(UserController.authenticate, PropertyController.deletePropertyById);
+.delete(PropertyController.deletePropertyById);
 
 // var PropertyController = require('../controllers/property.controller.js');
 // var UserController = require('../controllers/user.controller.js');
