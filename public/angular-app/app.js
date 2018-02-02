9782e3225fc87ebb9f1fe1634a0ccf1e69bb48fa
snuud <sn.snuud@gmail.com>
2018-02-02 16:41:16 +0800

    Added app config

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -1,1 +1,25 @@
-angular.module('app', ['ngRoute']);
+angular.module('app', ['ngRoute']).config(config);
+
+function config($routeProvider, $locationProvider, $httpProvider){
+    $httpProvider.interceptors.push('AuthInterceptor');
+    $locationProvider.hashPrefix('');
+    $routeProvider.
+        when('/', {
+            templateUrl: 'angular-app/home/home.html',
+            access: {
+                restricted: false
+            }
+        }).
+        when('/manageproperty', {
+            templateUrl: 'angular-app/manage-property/manage-property.html',
+            controller: ManagePropertyController,
+            controllerAs: 'vm',
+            access: {
+                restricted: true
+            }
+        }).
+        otherwise({
+            redirectTo : '/'
+        });
+
+};
---
+++ public/angular-app/manage-property/manage-property-controller.js
@@ -0,0 +1,7 @@
+angular.module('app').controller('ManagePropertyController', ManagePropertyController);
+
+function ManagePropertyController(){
+    var vm = this;
+
+
+};
