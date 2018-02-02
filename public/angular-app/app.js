c08f9bf2a7a9ec5f097e58c73e37f55302b3b6c9
snuud <sn.snuud@gmail.com>
2018-02-02 17:01:21 +0800

    Add links to all navigation menu

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -6,10 +6,26 @@ function config($routeProvider, $locationProvider, $httpProvider){
         when('/', {
             templateUrl: 'angular-app/home/home.html'
         }).
+        when('/property', {
+            templateUrl: 'angular-app/property/property.html'
+        }).
+        when('/buy', {
+            templateUrl: 'angular-app/property/buy.html'
+        }).
+        when('/rent', {
+            templateUrl: 'angular-app/property/rent.html'
+        }).
+        when('/about', {
+            templateUrl: 'angular-app/about/about.html'
+        }).
+        when('/contact', {
+            templateUrl: 'angular-app/contact/contact.html'
+        }).
+        when('/manageuser', {
+            templateUrl: 'angular-app/manage-user/manage-user.html'
+        }).
         when('/manageproperty', {
-            templateUrl: 'angular-app/manage-property/manage-property.html',
-            controller: ManagePropertyController,
-            controllerAs: 'vm'
+            templateUrl: 'angular-app/manage-property/manage-property.html'
         }).
         otherwise({
             redirectTo : '/'
--- public/angular-app/manage-property/manage-property-controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('app').controller('ManagePropertyController', ManagePropertyController);
-
-function ManagePropertyController(){
-    var vm = this;
-
-
-};
