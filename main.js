347f25915e94226a5a27eee116bdc3502d310f4c
snuud <sn.snuud@gmail.com>
2018-02-02 16:05:56 +0800

    Added body-parser to package.json

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ require('./api/data/dbconnection.js');
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var routes = require('./api/routes');
 
 var port = process.env.PORT || 8080;
 app.set('port', port);
+
+app.use(bodyParser.json());
 
 app.use('/api', routes);
 
