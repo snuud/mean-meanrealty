9664c320b416d40b89212db464ed99b408b8e92f
snuud <sn.snuud@gmail.com>
2018-02-02 15:23:33 +0800

    Add server port to main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Test succeeded!');
+var port = process.env.PORT || 8080;
+app.set('port', port);
+
+var server = app.listen(app.get('port'), function(){
+    var serverPort = server.address().port;
+    console.log('Server running on port ' + serverPort);
+});
