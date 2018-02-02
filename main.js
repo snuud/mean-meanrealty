16bb2361b7e00424ce7a756c5b31edc96e9f5b17
snuud <sn.snuud@gmail.com>
2018-02-02 16:13:06 +0800

    Added public folder for front end files

--- main.js
+++ main.js
@@ -2,12 +2,21 @@ require('./api/data/dbconnection.js');
 
 var express = require('express');
 var app = express();
+var path = require('path');
 var bodyParser = require('body-parser');
 
 var routes = require('./api/routes');
 
 var port = process.env.PORT || 8080;
 app.set('port', port);
+
+app.use(function(req, res, next){
+    console.log(req.method + ' ' + req.url);
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
 
 app.use(bodyParser.json());
 
