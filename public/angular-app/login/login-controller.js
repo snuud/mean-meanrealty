110246a89ba104af20e2b144385bcf40e8447722
snuud <sn.snuud@gmail.com>
2018-02-05 16:17:43 +0800

    x

--- public/angular-app/login/login-controller.js
+++ public/angular-app/login/login-controller.js
@@ -42,4 +42,18 @@ function LoginController($http, $location, $window, AuthFactory, jwtHelper){
         delete $window.sessionStorage.token;
         $location.path('/');
     }
+
+    $(document).on('click', function(event){
+      var $clickedOn = $(event.target),
+          $collapsableItems = $('.collapse'),
+          isToggleButton = ($clickedOn.closest('.navbar-toggle').length == 1),
+          isLink = ($clickedOn.closest('a').length == 1),
+          isOutsideNavbar = ($clickedOn.parents('.navbar').length == 0);
+
+      if( (!isToggleButton && isLink) || isOutsideNavbar ) {
+        $collapsableItems.each(function(){
+          $(this).collapse('hide');
+        });
+      }
+    });
 }
